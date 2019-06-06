dd4a8ebc0c5c42f51572eb71da6a8f66f8881d27
BenC0 <suddensquirrel94@gmail.com>
2019-06-06 12:41:34 +0100

    workshop template started

---
+++ workshop/code/src/index.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+import "./styles/index.scss";
+
+$(document).ready(function() {
+	$("body").addClass('page_loaded')
+});
---
+++ workshop/code/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+	entry: './src/index.js',
+	output: {
+		filename: 'script.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './src',
+		hot: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.html$/,
+				use: [
+					'html-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(["dist"]),
+		new HtmlWebpackPlugin({
+			title: 'Dentsu Data Labs',
+			template: './src/index.html'
+		}),
+		new webpack.HotModuleReplacementPlugin()
+	]
+};
+
+module.exports = config;
