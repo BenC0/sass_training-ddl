bed7edbc002f9a29c58132a89d860c169c3d4dd4
BenC0 <suddensquirrel94@gmail.com>
2019-06-07 10:10:15 +0100

    Form field focusout event handler implemented

--- workshop/code/src/index.js
+++ workshop/code/src/index.js
@@ -1,7 +1,17 @@
 import $ from "jquery";
 
-import "./styles/index.scss";
+import "./example-styles/index.scss";
 
 $(document).ready(function() {
 	$("body").addClass('page_loaded')
+
+	$('input').focusout(function(event) {
+		/* Act on the event */
+		let el = $(event.currentTarget)
+		if (el.val().replace(/ /g, '').length !== 0) {
+			el.addClass('active')
+		} else {
+			el.removeClass('active')
+		}
+	});
 });
